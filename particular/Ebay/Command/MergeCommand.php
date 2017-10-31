0135228fd22faf8ced9288f4f96223d69040b80a
johnk <johnk@riceball.com>
2017-10-31 10:02:18 -0700

    Merge now reads SKUs from STDIN

--- particular/Ebay/Command/MergeCommand.php
+++ particular/Ebay/Command/MergeCommand.php
@@ -17,7 +17,7 @@ class MergeCommand extends Command
         $this
             ->setName('merge')
             ->setDescription('Moves SKU directories into the first SKU directory listed on the command line. The order is the reverse of the "mv" command.')        
-            ->setHelp('SKU directories contain information and photos of the SKU. When individual items are combined into a "lot" for bulk sale, they can be combined into one SKU. Merging keeps the state directory clean by gathering photos into one directory.');
+            ->setHelp('SKU directories contain information and photos of the SKU. When individual items are combined into a "lot" for bulk sale, they can be combined into one SKU. Merging keeps the state directory clean by gathering photos into one directory. If only one argument, the target directory, is provided, then standard input is read for SKUs.');
 
         $this
             ->addArgument('targetSKU', InputArgument::REQUIRED, 'SKU that receives resources for the other SKUs.')
@@ -30,22 +30,26 @@ class MergeCommand extends Command
         $targetSKU = $input->getArgument('targetSKU');
         $skus = $input->getArgument('skus');
 
+        // if we provide only the target sku, we read stdin for skus
+        $read_stdin = (count($skus) == 0); 
+
+        // we use this to open nautilus
+        $path = COMMAND_DIR."/../".$this->locateState($targetSKU)."/$targetSKU/";
+
         foreach($skus as $sku) {
             $this->move($sku, $targetSKU);
         }
 
-        $path = COMMAND_DIR."/../".$this->locateState($targetSKU)."/$targetSKU/";
-        
-        exec("nautilus \"$path\"");
+        if ($read_stdin) {
+            while($line = fgets(STDIN)) {
+                $line = trim($line);
+                if (!$line) continue; // blank line
 
-        return;
-
-        while($line = fgets(STDIN)) {
-            $line = trim($line);
-            if (!$line) continue; // blank line
-
-            $this->move($line, $targetSKU);
+                $this->move($line, $targetSKU);
+            }
         }
+
+        exec("nautilus \"$path\"");
     }
 
     private function move($sku, $targetSKU) {
@@ -56,6 +60,11 @@ class MergeCommand extends Command
 
         if (file_exists($newpath)) {
             echo "Warning: $newpath exists. Not moving $line.\n";
+            return;
+        }
+
+        if ($sku == $targetSKU) {
+            echo "Cannot move to itself.\n";
             return;
         }
 
